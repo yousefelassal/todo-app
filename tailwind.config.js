7ca471e15f896b7d707bb3956bcc836901ec3160
yousef <yousefsameh24@gmail.com>
2023-06-01 21:24:56 +0300

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/**/*.html', './dist/**/*.js'],
+  content: ['./dist/**/*.html', './dist/**/*.js', 'src/**/*.html', 'src/**/*.js'],
   darkMode: 'class',
   theme: {
     extend: {},
