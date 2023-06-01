983435b2bfe6cd54c6f85cdfa6e02ee57f4e5cb5
yousef <yousefsameh24@gmail.com>
2023-06-01 15:25:18 +0300

    adjust configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,35 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    landing: './src/landing.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack 5 Boilerplate',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+      chunks: ['app'],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Webpack 5 Boilerplate',
+      template: path.resolve(__dirname, 'src', 'landing.html'),
+      filename: 'landing.html',
+      chunks: ['landing'],
+    }),
+  ],
   output: {
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    clean: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
   devServer: {
     static: {
