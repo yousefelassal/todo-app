578be41d3d9a9b6da376cdc43cc67d77c5f68430
yousef <yousefsameh24@gmail.com>
2023-06-05 16:26:33 +0300

    handle mobile nav

--- src/landing.js
+++ src/landing.js
@@ -1,1 +1,15 @@
 import './style.css'
+
+const navbar = document.getElementById('navbar');
+const openNav = document.getElementById('open-nav');
+const closeNav = document.getElementById('close-nav');
+
+openNav.addEventListener('click', () => {
+    navbar.classList.remove('hidden');
+    navbar.classList.add('flex');
+});
+
+closeNav.addEventListener('click', () => {
+    navbar.classList.remove('flex');
+    navbar.classList.add('hidden');
+});  
