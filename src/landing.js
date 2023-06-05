13dd225d8e6b89930771b95999606b132a9e71d0
yousef <yousefsameh24@gmail.com>
2023-06-05 17:24:55 +0300

    create testimonials

--- src/landing.js
+++ src/landing.js
@@ -1,4 +1,6 @@
 import './style.css'
+import Socrates from './images/socrates.jpg'
+import VladimirLenin from './images/Vladimir_Lenin.jpg'
 
 const navbar = document.getElementById('navbar');
 const openNav = document.getElementById('open-nav');
@@ -13,3 +15,9 @@ closeNav.addEventListener('click', () => {
     navbar.classList.remove('flex');
     navbar.classList.add('hidden');
 });  
+
+const socrates = document.getElementById('socrates');
+const lenin = document.getElementById('lenin');
+
+socrates.src = Socrates;
+lenin.src = VladimirLenin;
